6b8e842225dd21ef2f921f5ac826e0be960f65b0
Adam Krueger <akrueger@einsteinmedical.com>
2016-03-04 23:52:15 -0800

    Added files and setup javascript to parse JSON

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>JavaScript JSON Test</title>
+  <meta name="description" content="JavaScript JSON Test">
+  <meta name="author" content="Adam Krueger">
+
+  <script>
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "family.json");
+    request.send(null);
+    request.onreadystatechange = function() {
+      if ( request.readyState === 4 && request.status === 200 ) {
+        var my_JSON_object = JSON.parse(request.responseText);
+        console.log(my_JSON_object);
+        document.getElementById("output").innerHTML = my_JSON_object;
+      }
+    }
+
+  </script>
+
+</head>
+
+<body>
+
+  <h1>JavaScript JSON Test</h1>
+
+  <p>Below is the output of a JSON file parsed with JavaScript.</p>
+
+  <div id="output"></div>
+  
+</body>
+</html>
